b4d8d75d83e139f5bf839e18373f6ccda73aa43c
Colin Chan <colinchan@lumeh.org>
2011-12-20 17:01:50 -0500

    Add utility functions for displaying messages

---
+++ util.py
@@ -0,0 +1,13 @@
+import sys
+
+def warn(message, **kwargs):
+    _print_message(message, 'error', sys.stderr, **kwargs)
+
+def info(message, **kwargs):
+    _print_message(message, 'info', sys.stderr, **kwargs)
+
+def _print_message(message, type, file, *, line=None)
+    message = str(message)
+    if line:
+        message = "[type] {}: ".format(line) + message
+    print(message, file=file)
