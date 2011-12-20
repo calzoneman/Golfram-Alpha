1f7aea5ee9e79497ce25605a82dab50c7920a361
Colin Chan <colinchan@lumeh.org>
2011-12-20 16:15:30 -0500

    Make super-huge major changes (not quite finished)

--- core.py
+++ core.py
@@ -1,46 +1,68 @@
+"""The core classes for Golfram Alpha (Level, Tile, Ball, etc.)
+
+Doctests:
+>>> t = Tile(size=1)
+>>> level = Level([[t,t,t]] * 3)
+>>> coordinates = (3, 7)
+>>> level.get_tile(**coordinates) is t
+True
+
+"""
+import re
+
 import pygame
 from pygame.locals import *
 
+from golfram.util import warn, info
+
 class Level:
 
-    DEFAULT_TYPEID = 0
-
-    def __init__(self, width=1, height=1):
+    def __init__(self, tiles=None, *, width=1, height=1):
         self.width = width
         self.height = height
 
         self.tiletypes = {} # Dictionary {typeid : TileType() with that typeid}
-        self.tiles = [] # The level data
+        self._tiles = [] # The level data
         self.tilesize = 1 # The edge length of a tile, in pixels
 
-    def add_row(self):
-        self.tiles.append([Level.DEFAULT_TYPEID for a in range(self.width)])
+    def px(self, tile_units):
+        """Return the pixels equivalent of a dimension in tile units"""
+        return tile_units * self.tilesize
+
+    def append_row(self, tile=None):
+        """Append a row of the given tile to the level"""
+        if not tile:
+            tile = self._default_tile
+        self._tiles.append([tile] * self.width)
         self.height += 1
 
-    def add_rows(self, rows):
-        for a in range(rows):
-            self.add_row()
-
-    def add_column(self):
+    def append_column(self, tile=None):
+        """Append a column of the given tile to the level"""
+        if not tile:
+            tile = self._default_tile
+        for row in self._tiles:
+            row.append(tile)
         self.width += 1
-        for j in range(len(self.tiles)):
-            self.tiles[j].append(Level.DEFAULT_TYPEID)
 
-    def add_columns(self, cols):
-        for a in range(cols):
-            self.add_column()
+    def get_tile(self, row, column):
+        """Return the tile at the given coordinates
 
-    def get_at(self, xy): # Returns a TileType object
-        try:
-            tt = self.tiletypes[tiles[xy[1]][xy[0]]]
-        except IndexError:
-            tt = None
-        return tt
+        This will raise IndexError if the row and column are out of bounds.
 
-    def set_at(self, xy, t): # t is the typeid, not a TileType object
-        if xy[0] >= self.width or xy[1] >= self.height or xy[0] < 0 or xy[1] < 0:
-            return
-        self.tiles[xy[1]][xy[0]] = t
+        Hint: You can use tuple unpacking when you call the function.
+        coordinates = (x, y)
+        level.get_tile(**coordinates)
+
+        """
+        return self._tiles[row][column]
+
+    def set_tile(self, row, column, tile):
+        """Set the tile at the given coordinates
+
+        This will raise IndexError if the row and column are out of bounds.
+
+        """
+        self._tiles[row][column] = tile
 
     def expand_and_set(self, xy, t):
         if xy[0] >= self.width:
@@ -49,18 +71,105 @@ class Level:
             self.add_rows(xy[1] - self.height + 1)
         self.set_at(xy, t)
 
-    def draw(self, x_offset, y_offset, width, height):
-        surf = pygame.Surface((width * self.tilesize, height * self.tilesize), SRCALPHA)
-        x = x_offset
-        y = y_offset
-        while y < y_offset + height and y < self.height:
-            while x < x_offset + width and x < self.width:
-                surf.blit(self.tiletypes[self.tiles[y * self.width + x]].get_texture(), ((x - x_offset) * self.tilesize, (y - y_offset) * self.tilesize))
-                x += 1
-            y += 1
-            x = x_offset
-
+    def draw(self, row_start, column_start, rows, columns):
+        surface = pygame.Surface((self.px(rows), self.px(columns)), SRCALPHA)
+        row = row_start
+        column = column_start
+        while row < row_start + rows and row < self.rows:
+            while column < column_start + columns and column < self.columns:
+                surface.blit(self.get_tile(row, column).texture,
+                             (px(column - column_start), px(row - row_start)))
+                column += 1
+            row += 1
+            column = column_start
         return surf
+
+#    def draw(self, x_offset, y_offset, width, height):
+#        surf = pygame.Surface((width * self.tilesize, height * self.tilesize), SRCALPHA)
+#        x = x_offset
+#        y = y_offset
+#        while y < y_offset + height and y < self.height:
+#            while x < x_offset + width and x < self.width:
+#                surf.blit(self.tiletypes[self.tiles[y * self.width + x]].get_texture(), ((x - x_offset) * self.tilesize, (y - y_offset) * self.tilesize))
+#                x += 1
+#            y += 1
+#            x = x_offset
+#
+#        return surf
+
+    @staticmethod
+    load_file(filename):
+        """Create a Level object from a level file"""
+        # Stairs
+        tiles = {}
+        width = None
+        height = None
+        texture = None
+        tilesize = None
+        leveldata = []
+        # Read in the file. Don't catch IO exceptions here... it's up to the
+        # code that calls this to decide what to do.
+        f = open(filename, 'r')
+        lines = f.readlines()
+        f.close()
+        while ln < len(lines):
+            ln += 1
+            # Remove comments and split line into words based on whitespace
+            words = lines[ln][:line.find('#')].split()
+            if words[0] == '@width':
+                try:
+                    width = int(words[1])
+                except ValueError, IndexError:
+                    warn("Expected an integer for @width", line=ln)
+                else:
+                    info("Width: {}".format(width), line=ln)
+            elif words[0] == '@height':
+                try:
+                    height = int(words[1])
+                except ValueError, IndexError:
+                    warn("Expected an integer for @height", line=ln)
+                else:
+                    info("Height: {}".format(height), line=ln)
+            elif words[0] == '@tt':
+                params = {}
+                for word in words[1:]:
+                    t = word.split('=')
+                    params[t[0]] = t[1]
+                try:
+                    char = params['char']
+                except KeyError:
+                    warn("Expected a char= statement for @tt", line=ln)
+                else:
+                    del params['char']
+                    tiles[char] = Tile(**params)
+                    info("Tiletype: char={} {}".format(char, params), line=ln)
+            elif words[0] == '@texture':
+                try:
+                    texture = pygame.image.load(words[1])
+                except pygame.error as e:
+                    warn("Couldn't load texture {}".format(words[1]), line=ln)
+                    warn(e)
+                else:
+                    info("Texture: {}".format(texture), line=ln)
+            elif words[0] == '@tilesize':
+                try:
+                    tilesize = int(words[1])
+                except ValueError:
+                    warn("Expected an integer for @tilesize", line=ln)
+                else:
+                    info("Tilesize: {}".format(tilesize), line=ln)
+            elif words[0] == '@leveldata':
+                # Eat all of the leveldata lines
+                try:
+                    while not lines[ln+1].trim().startswith('@'):
+                        ln += 1
+                        leveldata.append(lines[ln].trim())
+                except IndexError:
+                    # We've reached the end of the file
+                    pass
+            else:
+                warn("Ignoring unexpected line", line=ln)
+
 
     @staticmethod
     def load_file(filename):
