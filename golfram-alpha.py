b8897ba4d2e8204a122253a37b0c4e238eb83904
Colin Chan <colinchan@lumeh.org>
2012-01-14 00:50:00 -0500

    Outline main program code, command line parsing

---
+++ golfram-alpha.py
@@ -0,0 +1,49 @@
+import argparse
+import pickle
+from random import choice
+
+import pygame
+
+from golfram.core import Level
+
+# Some constants that should maybe eventually be relocated.
+# Maybe to a Settings class.
+GOLFRAM_ALPHA = 'Golfram Alpha'
+VERSION = 'golfram-alpha-0.1'
+RESOLUTION = [640,480]
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description="Play a nice game of minigolf.")
+parser.add_argument('-v', '--version', action='version', version=VERSION)
+parser.add_argument('-l', '--levelset',
+                    help="the levelset file you wish to play")
+parser.add_argument('--bunny', dest='bunny', action='store_true')
+args = parser.parse_args()
+
+if not args.bunny and not args.levelset:
+    parser.print_usage()
+
+# Print a bunny, if requested
+if args.bunny:
+    try:
+        bunnies = pickle.load(open('bunnies', 'r'))
+    except:
+        print("Bunnies are unavailable. No further information is available " +
+              "because the code in this area is hacked together and uses a " +
+              "bare except clause.")
+    else:
+        print(choice(bunnies))
+
+# Load the specified levelset, if requested
+if args.levelset:
+    # Create game object, load levels, whatever...
+
+    # Set up a basic pygame window
+    pygame.init()
+    screen = pygame.display.set_mode(RESOLUTION)
+    pygame.display.set_caption(GOLFRAM_ALPHA)
+
+    # do stuff
+
+    # exit
+    pygame.quit()
