6d608c88cb1c057fa8e5b57d8a8cc96f786d2e7b
Colin Chan <colinchan@lumeh.org>
2012-01-14 11:32:08 -0500

    Add doctests for util printing functions
    Also:
     * Stop specifying sys.stderr or sys.stdout by default
     * Remove old basename call from _print_message

--- golfram/util.py
+++ golfram/util.py
@@ -3,18 +3,36 @@ import os
 import sys
 
 def warn(message, **kwargs):
-    """Print a message explaining a non-critical error"""
-    _print_message(message, 'warn', sys.stderr, **kwargs)
+    """Print a message explaining a non-critical error
+
+    >>> warn("Blah!")
+    warn: Blah!
+    >>> warn("ignoring duplicate declaration", file='input.txt', line=42)
+    [input.txt:42] warn: ignoring duplicate declaration
+
+    """
+    _print_message(message, 'warn', **kwargs)
 
 def info(message, **kwargs):
-    """Print a message explaining normal operation"""
-    _print_message(message, 'info', sys.stdout, **kwargs)
+    """Print a message explaining normal operation
+
+    >>> info("everything works!")
+    info: everything works!
+
+    """
+    _print_message(message, 'info', **kwargs)
 
 def error(message, **kwargs):
-    """Print a message explaining a critical error"""
-    _print_message(message, 'error', sys.stderr, **kwargs)
+    """Print a message explaining a critical error
 
-def _print_message(message, message_type, output_file, line=None, file=None):
+    >>> error("can't open level: {}".format('level42.py'))
+    error: can't open level: level42.py
+
+    """
+    _print_message(message, 'error', **kwargs)
+
+def _print_message(message, message_type, output_file=None, line=None,
+                   file=None):
     if file:
         file = os.path.basename(file)
     if line and file:
@@ -23,7 +41,7 @@ def _print_message(message, message_type, output_file, line=None, file=None):
         template = '[{file}] {type}: {message}'
     else:
         template = '{type}: {message}'
-    message = template.format(type=message_type, file=basename(file),
+    message = template.format(type=message_type, file=file,
                               line=line, message=message)
     print(message, file=output_file)
 
