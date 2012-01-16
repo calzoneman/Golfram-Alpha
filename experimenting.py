bbd46cbe3503463b6fd0e40088399f29afa27c02
Colin Chan <colinchan@lumeh.org>
2012-01-15 23:49:27 -0500

    Add more comments to experimenting.py

--- experimenting.py
+++ experimenting.py
@@ -5,6 +5,7 @@ import pygame
 
 from golfram.core import Level, Tile
 
+# Load tile textures and make tiles
 sred = pygame.image.load('sprites/red.png')
 sgreen = pygame.image.load('sprites/green.png')
 sblue = pygame.image.load('sprites/blue.png')
@@ -12,12 +13,13 @@ red = Tile(texture=sred)
 blue = Tile(texture=sblue)
 green = Tile(texture=sgreen)
 
+# Create a level of 6x6 random tiles
 N = 6
 choices = [red, green, blue]
 tiles = [[choice(choices) for x in range(N)] for y in range(N)]
-
 level = Level(tiles=tiles, tilesize=64, width=192, height=192)
 
+# Load the ball texture
 ball = pygame.image.load('sprites/ball-12x12.png')
 ball_position = [0, 0]
 
@@ -30,15 +32,18 @@ background = pygame.Surface(screen.get_size()).convert()
 level.draw_on_surface(background)
 
 # Event loop
-pygame.time.set_timer(pygame.USEREVENT + 1, 30)
+BALL_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(BALL_EVENT, 30)
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.USEREVENT + 1:
+        if event.type == BALL_EVENT:
+            # Move ball
             ball_position[0] = (ball_position[0] + 1) % 640
             ball_position[1] = (ball_position[1] + 1) % 480
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    # Update screen
     screen.blit(background, dest=(0, 0))
     screen.blit(ball, dest=ball_position)
     pygame.display.flip()
